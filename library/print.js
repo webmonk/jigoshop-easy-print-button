8d2b0c5e841e349d390ec58d3338834926934b89
webmonk <scopes_oye@yahoo.com>
2013-09-07 21:05:59 -0700

    Create print.js

---
+++ library/print.js
@@ -0,0 +1,7 @@
+function PrintDiv() {    
+           var divToPrint = document.getElementById('order_customer_billing_data');
+           var popupWin = window.open('', '_blank', 'width=450,height=500');
+           popupWin.document.open();
+           popupWin.document.write('<html><head>'+'<link rel="stylesheet" href="http://codemypain.com/print.css">'+'</head><body onload="window.print()"><div id="main">' + divToPrint.innerHTML + '</div></body></html>');
+           popupWin.document.close();
+}
